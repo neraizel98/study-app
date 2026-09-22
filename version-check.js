04b41ee47ae35995aed2992863feb8ae85dcdebb
neraizel98 <neraizel98@nx3games.com>
2026-09-22 11:50:04 +0900

    Align update banner version with deployed parent settings release

--- version-check.js
+++ version-check.js
@@ -9,7 +9,7 @@
     if (window._versionCheckDone) return;
     window._versionCheckDone = true;
 
-    const APP_VERSION = '20260920-syncstatus';
+    const APP_VERSION = '20260921-parentplan';
 
     (async function () {
         try {
